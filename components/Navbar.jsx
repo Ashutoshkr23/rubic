8d4fafe4693b3a1644d0d8f82a0379e60d34fb28
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-01-27 16:11:00 +0530

    minor-ui-fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -73,7 +73,7 @@ function Navbar() {
         
 
         <div>
-          <label className="btn btn-circle swap swap-rotate">
+          <label className="btn btn-circle bg-transparent border-0 swap swap-rotate">
             <input type="checkbox" />
             <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
             <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
@@ -90,7 +90,7 @@ function Navbar() {
           <img className="mr-10 h-10 w-32" src='./rubic-logo.svg' />
 
           <div>
-            <label className="btn btn-circle swap swap-rotate">
+            <label className="btn btn-circle bg-transparent border-0 swap swap-rotate">
               <input type="checkbox" />
               <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
               <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
@@ -98,7 +98,7 @@ function Navbar() {
             </label>
           </div>
           <div className="absolute inset-x-1/4" style={{ width: '565px', }}>
-            <button className='btn px-5 py-3.5 text-white rounded-full bg-bg-black'>Swaps</button>
+            <button className='btn px-5 border-0 py-3.5 text-white rounded-full bg-bg-black'>Swaps</button>
           </div>
           <button className='w-44 h-12 px-9 py-3 ml-auto bg-bg-grey rounded-full text-white'> <img className='w-5 h-6' src='./rubic-light.svg' /><div className='ml-2'>Buy RBC</div></button>
 
